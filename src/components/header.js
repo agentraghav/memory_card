97772e4600764265884434bd3606cc4bb3d70711
agentraghav <raghvendrarathore2@gmail.com>
2021-01-14 16:32:04 +0530

    update

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ import { Image } from 'react-bootstrap';
 export const Header = () => {
   return (
     <>
-      <Navbar bg='dark' variant='dark'>
+      <Navbar className='nav-back'>
         <Container>
           <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
           <Nav className='mr-auto'>
@@ -15,10 +15,6 @@ export const Header = () => {
             <Nav.Link href='#features'>Features</Nav.Link>
             <Nav.Link href='#pricing'>Pricing</Nav.Link>
           </Nav>
-          <Form inline>
-            <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-            <Button variant='outline-info'>Search</Button>
-          </Form>
         </Container>
       </Navbar>
     </>
