abdfbbb4b1525efc0b6b359f3ed129021b7c2806
agentraghav <raghvendrarathore2@gmail.com>
2021-01-14 17:48:50 +0530

    functioning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Header } from './components/header';
+import { Game } from './components/game';
 function App() {
   return (
     <>
       <Header />
+      <Game />
     </>
   );
 }
---
+++ src/components/game.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+export const Game = () => {
+  return (
+    <>
+      <Row className='back'></Row>
+    </>
+  );
+};
