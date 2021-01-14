b7df3efd36ca390f2041fd2d632e07bb5a31f3ab
agentraghav <raghvendrarathore2@gmail.com>
2021-01-14 16:59:08 +0530

    header

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import '../styles/styles.css';
-import { Nav, Container } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
-import { Form, FormControl, Button } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
 export const Header = () => {
   return (
     <>
-      <Navbar className='nav-back'>
-        <Container>
-          <Image
-            className='head-img'
-            src='https://media.harrypotterfanzone.com/harry-potter-logo-tutorial-final.jpg'></Image>
-          <div className='head-nav'>
-            <h1>Memory Card</h1>
-          </div>
-        </Container>
-      </Navbar>
+      <Row className='nav-back'>
+        <Image
+          className='head-img'
+          src='https://media.harrypotterfanzone.com/harry-potter-logo-tutorial-final.jpg'></Image>
+        <div className='head-nav'>
+          <h1>Memory Card</h1>
+        </div>
+      </Row>
     </>
   );
 };
