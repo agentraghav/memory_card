8506aa76fa12a7e4f07e554daadb270052405cd0
agentraghav <raghvendrarathore2@gmail.com>
2021-01-14 17:58:53 +0530

    game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useState, useEffect } from 'react';
 import { Header } from './components/header';
 import { Game } from './components/game';
 function App() {
+  const [score, setScore] = useState(0);
+  const [highScore, sethighScore] = useState(0);
+
+  const increaseScore = () => {
+    setScore((prevScore) => prevScore + 1);
+  };
+
+  const bestScore = (v) => {
+    if (highScore < v) {
+      sethighScore(v);
+    }
+  };
+
+  const resetScore = () => {
+    setScore(0);
+  };
+
   return (
     <>
       <Header />
-      <Game />
+      <Game
+        increaseScore={increaseScore}
+        bestScore={bestScore}
+        resetScore={resetScore}
+      />
     </>
   );
 }
